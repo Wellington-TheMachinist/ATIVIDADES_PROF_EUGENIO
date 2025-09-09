d1cf1a9c432a42deab922f9b252d3647f2dc22f9
WELLINGTON S R JUNIOR <well737ng@gmail.com>
2025-09-09 20:52:26 -0300

    Update lista_1_exercicio_4.js

---
+++ FUNCOES/lista_1_exercicio_4.js
@@ -0,0 +1,8 @@
+basePiramide = 5;
+alturaPiramide = 5;
+
+function volumePiramide(valor1,valor2){
+    return 1/3*valor1*valor2;
+}
+
+console.log("O volume da pirâmide corresponde a :" + volumePiramide(basePiramide,alturaPiramide));
